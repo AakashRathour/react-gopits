97a383b1563ec83b39706a20a65af886ef296669
AakashRathour <aakashrathour@tractorjunction.com>
2022-11-23 12:07:42 +0530

    React Gopist Setup

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import About from "./about";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      okik0k0k
+    </>
+  );
+};
+export default Home;
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h2>About js</h2>
+      989494984
+    </>
+  );
+};
+export default About;
